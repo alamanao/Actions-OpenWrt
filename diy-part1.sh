2cd4ed516cf4db925703bee5732001b50a45188c
alamanao <46837564+alamanao@users.noreply.github.com>
2022-02-09 22:51:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,5 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-echo 'src-link custom /usr/src/openwrt/custom-feed' >>feeds.conf.default
+echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr.git' >>feeds.conf.default
 
